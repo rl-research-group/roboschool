d41392141ade31fe955a0f20e94c3b88d4f55e21
Dylan <dylanangus@hotmail.com>
2019-03-05 11:04:25 +0000

    Fixed bug

--- gravity_render_script.py
+++ gravity_render_script.py
@@ -6,7 +6,7 @@ import sys
 env_name=sys.argv[1]
 print("Using environment {}".format(env_name))
 env = gym.make('RoboschoolWalker2d-v1')
-envm = gym.wrappers.Moniter(env, env_name, video_callable=lambda x: True, force=True)
+envm = gym.wrappers.Monitor(env, env_name, video_callable=lambda x: True, force=True)
 env.reset()
 
 print("Before Gravity Change")
