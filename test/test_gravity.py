1985e6abaaa94be05d53fffed7d1c5371bb92f64
Dylan Angus <dylanangus@hotmail.com>
2019-03-15 18:05:49 +0000

    using roboschool zoo agent

--- test/test_gravity.py
+++ test/test_gravity.py
@@ -3,9 +3,16 @@ import numpy as np
 from time import sleep
 import sys
 from agent_zoo.RoboschoolWalker2d_v1_2017jul import ZooPolicyTensorflow
+import tensorflow as tf
 
 env_name=sys.argv[1]
 print("Using environment {}".format(env_name))
+config = tf.ConfigProto(
+    inter_op_parallelism_threads=1,
+    intra_op_parallelism_threads=1,
+    device_count = { "GPU": 0 } )
+sess = tf.InteractiveSession(config=config)
+
 env = gym.make('RoboschoolWalker2d-v1')
 agent = ZooPolicyTensorflow("Test_gravity1", env.observation_space, env.action_space)
 
@@ -14,24 +21,20 @@ obs0 = envm.reset()
 score = 0
 
 print("Before Gravity Change")
-for i in range(100):
-    agent.act(obs_data=obs0 , cx=None)
+done=False
+while not done:
+    action = agent.act(obs_data=obs0 , cx=None)
     obs1, r, done, _ = envm.step(action)
     obs0=obs1
     score += r
-    if done:
-        break
-    sleep(0.1)
-    envm.render("human")
+    # envm.render("human")
 
 obs0=envm.reset()
-envm.env.env.scene.cpp_world.set_gravity(0.0)
+envm.env.env.scene.cpp_world.set_gravity(100000)
 print("After Gravity Change")
-for i in range(100):
-    agent.act(obs_data=obs0 , cx=None)
+done=False
+while not done:
+    action = agent.act(obs_data=obs0 , cx=None)
     obs1, r, done, _ = envm.step(action)
     obs0=obs1
-    if done:
-        break
-    sleep(0.1)
-    envm.render()
+    # envm.render()
